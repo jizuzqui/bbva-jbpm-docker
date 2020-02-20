3ac6b78a9a61de54c5ec6ebe53a6ff8f96a985d1
Jaime <jaime@localhost.localdomain>
2020-02-20 01:30:55 +0100

    v0.2

--- run_jbpm_community.sh
+++ run_jbpm_community.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 --name jbpm-bbva-holding jbpm-bbva-holding:0.1
-# 	-v /opt/jboss/wb_git:/opt/jboss/wildfly/bin/.niogit:Z \
+JBPM_PERSISTENT_DIR=/home/jaime/development/docker/jbpm
+
+docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 \
+    -v $JBPM_PERSISTENT_DIR/logs/:/opt/jboss/wildfly/standalone/log/:Z \
+    -v $JBPM_PERSISTENT_DIR/repositories/mvn_home/:/opt/jboss/.m2/:Z \
+    -v $JBPM_PERSISTENT_DIR/repositories/:/opt/jboss/repositories/:Z \
+    -v $JBPM_PERSISTENT_DIR/data/:/opt/jboss/data/:Z \
+    -d --name jbpm-bbva-holding jbpm-bbva-holding:0.1
